295824804602b2c23c2f6a4eb60813a553230d66
tundeiness <tundeiness@yahoo.co.uk>
2019-08-07 13:21:41 +0100

    using ROUND function to round up gdp to the nearest $1000

--- 02_select_from_world.sql
+++ 02_select_from_world.sql
@@ -68,3 +68,15 @@ For South America show population in millions and GDP in billions both to 2 deci
 SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2)
 FROM world
 WHERE continent = 'South America'
+
+
+/*
+Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+
+Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+*/
+
+
+SELECT name, ROUND(gdp/population, -3)
+FROM world
+WHERE gdp >= 1000000000000
